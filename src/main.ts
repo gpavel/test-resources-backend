ac3b76a6161f985e58720f177dc60908968a2654
hpavel <gppavel94@gmail.com>
2021-01-05 16:28:30 +0200

    feat: add CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import { config } from 'dotenv';
 
 import { FoleonAuthManager } from './utils/FoleonTokenManager';
 import { ExpressProxyController } from './controllers/AuthProxyController';
+// import { StubAuthManager } from './utils/StubAuthManager';
 
 const DEFAULT_PORT = 3000;
 
 function initializeDefaultConfig(): Record<string, any>  {
-  return config({ path: path.resolve(__dirname + '../.env.default') }).parsed ?? {};
+  return config({ path: path.resolve(__dirname, '../.env.default') }).parsed ?? {};
 }
 
 function initializeUserConfig(): Record<string, any> {
@@ -32,10 +34,22 @@ export function main(): void {
 
   const controller = new ExpressProxyController(
     tokenManager,
+    // new StubAuthManager(),
   );
 
+  app.use(cors({ origin: (configuration.CLIENT_ORIGIN as string).split(',') }));
+  app.use('/test', controller.proxy('http://localhost:3000/hello-world'));
   app.use('/publications', controller.proxy('https://api.foleon.com/v2/magazine/edition'));
   app.use('/projects', controller.proxy('https://api.foleon.com/v2/magazine/title'));
+
+  app.use('/hello-world', (req, res) => {
+    res.status(200).json({
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      params: req.params,
+    });
+  })
 
   app.listen(port);
   console.log(`Server has been launched on ${port} port.`);
